b8306047a98b72e4f012d79ca8e7642e23f641f1
fcz <fangchizheng@mail.ustc.edu.cn>
2024-05-12 23:35:51 +0800

    bug fixed && output

--- lab6/cpu.cu
+++ lab6/cpu.cu
@@ -1,22 +1,20 @@
 #include <bits/stdc++.h>
 #include <opencv2/opencv.hpp>
+#include <ostream>
 
 struct Point {
-    int x0, y0;     //像素坐标
     double x, y, z; // RGB
-    Point(int x0 = 0, int y0 = 0, int x = 0, int y = 0,
-          int z = 0) :
-        x0(x0),
-        y0(y0), x(x), y(y), z(z) {
+    Point(double x = 0, double y = 0, double z = 0) :
+        x(x), y(y), z(z) {
     }
     Point operator-(const Point &b) {
-        return Point(0, 0, x - b.x, y - b.y, z - b.z);
+        return Point(x - b.x, y - b.y, z - b.z);
     }
     Point operator+(const Point &b) {
-        return Point(0, 0, x + b.x, y + b.y, z + b.z);
+        return Point(x + b.x, y + b.y, z + b.z);
     }
     Point operator*(double k) {
-        return Point(0, 0, x * k, y * k, z * k);
+        return Point(x * k, y * k, z * k);
     }
     Point operator+=(const Point &b) {
         x += b.x;
@@ -39,32 +37,36 @@ double Kernel(double x, double h) {
 
 int main() {
     cv::Mat image = cv::imread("input.jpg");
+    cv::Mat dst_image = image.clone();
 
     int n = image.size().height, m = image.size().width;
-    Point *src_array = new Point[n * m];
-    Point *dst_array = new Point[n * m];
+    int N = n * m;
+    Point *src_array = new Point[N];
+    Point *dst_array = new Point[N];
 
-    int h = 10;
+    int h = 2;
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             cv::Vec3b pixel = image.at<cv::Vec3b>(i, j);
             src_array[i * m + j] =
-                Point(i, j, pixel[0], pixel[1], pixel[2]);
+                Point(pixel[0], pixel[1], pixel[2]);
         }
     }
 
-    int N = n * m;
+    std::cout << N << std::endl;
     for (int i = 0; i < N; i++) {
         Point p = src_array[i];
         Point nxt = p;
+        // if (i == 18856)
         if (i % 100 == 0)
-            std::cout << i << " " << p.x0 << " " << p.y0
+            std::cout << i << " " << i / m << " " << i % m
                       << " " << p.x << " " << p.y << " "
                       << p.z << std::endl;
 
         do {
             p = nxt;
+            nxt = Point(0, 0, 0);
             double sum = 0;
             for (int j = 0; j < N; j++) {
                 Point delta = src_array[j] - p;
@@ -75,20 +77,19 @@ int main() {
                 sum += w;
                 nxt += delta * w;
             }
-            nxt = nxt * (1 / sum);
-            // std::cout << nxt.x << " " << nxt.y << " "
-            //           << nxt.z << " " << dis(p, nxt)
-            //           << std::endl;
-
+            nxt = nxt * (1 / sum) + p;
         } while (dis(p, nxt) > 0.1);
 
         dst_array[i] = nxt;
+        if (i % 100 == 0)
+            std::cout << i << " " << i / m << " " << i % m
+                      << " " << p.x << " " << p.y << " "
+                      << p.z << std::endl;
     }
 
-    cv::Mat dst_image(n, m, CV_8UC3);
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            Point p = src_array[i * m + j];
+            Point p = dst_array[i * m + j];
             int x = p.x + 0.5, y = p.y + 0.5, z = p.z + 0.5;
             x = std::min(255, std::max(0, x));
             y = std::min(255, std::max(0, y));
